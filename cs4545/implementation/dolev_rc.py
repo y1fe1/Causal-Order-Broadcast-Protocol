d526260214a8056ad4c468aa6fda281bcdc30fff
Odyssey <2450587372@qq.com>
2024-11-27 22:52:05 +0100

    allow multiple broadcasters

--- cs4545/implementation/dolev_rc.py
+++ cs4545/implementation/dolev_rc.py
@@ -15,19 +15,35 @@ from ..system.da_types import ConnectionMessage
 )  # The value 1 identifies this message and must be unique per community.
 class DolevMessage:
     message: str
-    # id: Tuple[int, int]
+    message_id: int
     path: List[int]
 
 class BasicDolevRC(DistributedAlgorithm):
     
     def __init__(self, settings: CommunitySettings) -> None:
         super().__init__(settings)
-        self.f = 3
+        
+        # hardcoded here
+        self.f = 2
         self.starter_nodes = [3, 4, 2]
-        self.delivered = False
-        self.paths: set[tuple] = set()
+        self.malicious_nodes = [0, 7]
+
+       # self.is_malicious: bool = (self.node_id in self.malicious_nodes) 
+
+        self.is_delivered: dict[int, bool] = {}
+        self.message_paths: dict[int, set[tuple]] = {}
+        self.message_broadcast_cnt = 0
+
         self.add_message_handler(DolevMessage, self.on_message)
 
+    def generate_message_id(self, msg: str) -> int:
+        self.message_broadcast_cnt += 1
+        return self.node_id * 37 + self.message_broadcast_cnt + hash(msg)
+    
+    def generate_message(self) -> DolevMessage:
+        msg =  ''.join(random.choice(['Y', 'M', 'C', 'A']))
+        id = self.generate_message_id(msg)
+        return DolevMessage(msg, id, [])
 
     async def on_start(self):
         # print(f"[Node {self.node_id}] Starting algorithm with peers {[x.address for x in self.get_peers()]} and {self.nodes}")
@@ -48,19 +64,17 @@ class BasicDolevRC(DistributedAlgorithm):
 
     async def on_start_as_starter(self):
         # By default we broadcast a message as starter, but everyone should be able to trigger a broadcast as well.
-        self.delivered = False
-        self.paths = set()
-        message = f"Hello world from {self.node_id}"
 
+        message = self.generate_message()
         await self.on_broadcast(message)
 
 
-    async def on_broadcast(self, message: str) -> None:
+    async def on_broadcast(self, message: DolevMessage) -> None:
         # Assuming everything has been set up well for this node (delivered, paths, ...)
         print(f"Node {self.node_id} is starting Dolev's protocol")
         
         for peer in self.get_peers():
-            self.ez_send(peer, DolevMessage(message, []))
+            self.ez_send(peer, message)
 
         await self.trigger_delivery(message)
 
@@ -68,37 +82,39 @@ class BasicDolevRC(DistributedAlgorithm):
     async def on_message(self, peer: Peer, payload: DolevMessage) -> None:
         try:
             sender_id = self.node_id_from_peer(peer)
-            print(f"[Node {self.node_id}] Got message from node: {sender_id} with path {payload.path}")
+            # print(f"[Node {self.node_id}] Got message from node: {sender_id} with path {payload.path}")
 
             new_path = payload.path + [sender_id]
-            self.paths.add(tuple(new_path))
+            #self.paths.add(tuple(new_path))
+            self.message_paths.setdefault(payload.message_id, set()).add(tuple(new_path))
 
             for neighbor in self.get_peers():
                 neighbor_id = self.node_id_from_peer(neighbor)
                 if neighbor_id != self.node_id and neighbor_id not in new_path:
-                    self.ez_send(neighbor, DolevMessage(payload.message, new_path))
+                    print(f"[Node {self.node_id}] Sent message to node {neighbor_id} with path {new_path} : {payload.message_id}")
+                    self.ez_send(neighbor, DolevMessage(payload.message, payload.message_id, new_path))
 
-            if len(self.paths) >= (self.f + 1):
-                if not self.delivered and self.find_disjoint_paths_ok():
+            if len(self.message_paths.get(payload.message_id)) >= (self.f + 1):
+                if not self.is_delivered.get(payload.message_id) and self.find_disjoint_paths_ok(payload.message_id):
                     # print(f"Node {self.node_id} has enough node-disjoint paths, delivering message: {payload.message}")
-                    self.delivered = True
+                    self.is_delivered[payload.message_id] = True
                     await self.trigger_delivery(payload.message)
            
         except Exception as e:
             print(f"Error in on_message: {e}")
             raise e
 
-    async def trigger_delivery(self, message: str):
-        print(f"Node {self.node_id} delivering message: {message}")
-        self.stop()
-        # TODO: let it stop somewhere else
+    async def trigger_delivery(self, message: DolevMessage):
+        print(f"Node {self.node_id} delivering message: {message.message}")
+        self.is_delivered[message.message_id] = True
+
     
-    def find_disjoint_paths_ok(self) -> bool:
-        if not self.paths:
+    def find_disjoint_paths_ok(self, msg_id) -> bool:
+        if not self.message_paths.get(msg_id):
             return False
         disjoint_paths = []
         used_nodes = set()
-        paths_sorted = sorted(self.paths, key=len)
+        paths_sorted = sorted(self.message_paths.get(msg_id), key=len)
         
         for path in paths_sorted:
             path = path[1:]
