f5004b665a870c41f0f076065780a53ba3ba99cb
y1fe1 <42345009+y1fe1@users.noreply.github.com>
2024-12-03 21:25:27 +0100

    [A1: Add msg_cnt and byte_sent to output]

--- cs4545/implementation/dolev_rc.py
+++ cs4545/implementation/dolev_rc.py
@@ -398,7 +398,14 @@ class BasicDolevRC(DistributedAlgorithm):
         self.metrics.latency = self.metrics.end_time.get(msg_id) - start_time
         
     def write_metrics(self):
-        metrics_log = f"{self.node_id},{self.metrics.node_count},{self.metrics.byzantine_count},{self.metrics.connectivity},{self.metrics.latency:.3f},{self.metrics.message_count - self.metrics.last_message_count}"
+        metrics_log = f"{self.node_id},
+                        {self.metrics.node_count},
+                        {self.metrics.byzantine_count},
+                        {self.metrics.connectivity},
+                        {self.metrics.latency:.3f},
+                        {self.metrics.message_count - self.metrics.last_message_count},
+                        {len(self._message_history)},
+                        {self._message_history.bytes_sent()}"
         self.metrics.last_message_count = self.metrics.message_count
         with open("output/metrics_output.csv", "a") as f:
             f.write(metrics_log + "\n")
