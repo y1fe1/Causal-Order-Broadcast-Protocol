8e14eda0704526e8f1e2bad4acd923f5b6be5263
Haoran Tang <h.tang-6@student.tudelft.nl>
2024-12-10 01:56:23 +0100

    fix: Improve some implementation

--- cs4545/implementation/bracha_rb.py
+++ cs4545/implementation/bracha_rb.py
@@ -13,13 +13,15 @@ from hashlib import sha256
 
 from cs4545.system.da_types import DistributedAlgorithm, message_wrapper
 from cs4545.implementation.dolev_rc_new import BasicDolevRC, MessageConfig, DolevMessage
-# from cs4545.implementation.node_log import message_logger, OutputMetrics, LOG_LEVL
+from cs4545.implementation.node_log import message_logger, OutputMetrics, LOG_LEVEL
 
 class BrachaConfig(MessageConfig):
     def __init__(self, broadcasters = {1:2, 2:1}, malicious_nodes = [3],N = 10, msg_level = logging.DEBUG):
         super().__init__(broadcasters, malicious_nodes,N,msg_level)
-
-#region Message Definition
+        
+"""
+region Message Definition
+"""
 class MessageType(Enum):
     SEND = 1
     ECHO = 2
@@ -29,36 +31,23 @@ class MessageType(Enum):
     msg_id=4
 ) 
 class SendMessage(DolevMessage):
-
     msg_type = MessageType.SEND
-
-    def __init__(self, message: str, source_id:int, path , message_id: str = "3", msg_type = MessageType.SEND):
-        super().__init__(message, message_id, source_id, path)
-        self.msg_type = msg_type
 
 @dataclass(
     msg_id=5
 ) 
 class EchoMessage(DolevMessage):
-
     msg_type = MessageType.ECHO
-
-    def __init__(self, message: str, source_id:int, path , message_id: str = "3", msg_type = MessageType.ECHO):
-        super().__init__(message, message_id, source_id, path)
-        self.msg_type = msg_type
 
 @dataclass(
     msg_id=6
 ) 
 class ReadyMessage(DolevMessage):
-
     msg_type = MessageType.READY
 
-    def __init__(self, message: str, source_id, path, message_id: str = "3", msg_type = MessageType.READY):
-        super().__init__(message, message_id, source_id, path)
-        self.msg_type = msg_type
-
-#endregion
+"""
+endregion
+"""
 
 class BrachaRB(BasicDolevRC):
     def __init__(self, settings: CommunitySettings, parameters = BrachaConfig()) -> None:
@@ -95,40 +84,83 @@ class BrachaRB(BasicDolevRC):
         await super().on_start()
 
     async def on_start_as_starter(self):
-        return await super().on_start_as_starter()
+        await super().on_start_as_starter()
     
     # event <Bracha, Broadcast | M >  do
     async def on_broadcast(self, message: DolevMessage) -> None:
         #⟨Dolev,Broadcast|[Send,m]⟩
         super().on_broadcast(message)
 
-    #trigger ⟨al,Send | q,[ECHO,m]⟩ 
-    async def trigger_send_echo(self, message_id: str, count: int, threshold: int, payload: SendMessage):
 
-        sent_echo = self.is_echo_sent.get(message_id, {}).get(self.node_id, False)
-
-        if not sent_echo and count >= threshold:
-
-            self.is_echo_sent.setdefault(message_id, {})[self.node_id] = True
-
-            self.msg_log.log(LOG_LEVL.DEBUG, f'Sent ECHO messages: {message_id}')
-
-        # Broadcast ECHO message to peers ⟨Dolev,Broadcast|[Echo,m]⟩
-        super().on_broadcast(EchoMessage(payload.message, payload.message_id, self.node_id, []))
-
-    
-    #event ⟨al,Deliver | p,[SEND,m]⟩
+    """
+    Message Handlers
+    """
+    # event ⟨al,Deliver | p,[SEND,m]⟩
     @message_wrapper(SendMessage)
     async def on_send(self, peer: Peer, payload: SendMessage):
 
-        self.msg_log.log(LOG_LEVL.DEBUG, f'Received a SEND  message: {payload.message_id}.')
+        self.msg_log.log(LOG_LEVEL.DEBUG, f'Received a SEND message: {payload.message_id}.')
 
         message_id = payload.message_id
         
         # upon event ⟨al,Deliver | p,[SEND,m]⟩ and not sentEcho do
-        threshold = math.ceil((self.f + self.N + 1) / 2)
-        await self.trigger_send_echo(message_id, self.echo_count[message_id], threshold, payload)
+        # threshold = math.ceil((self.f + self.N + 1) / 2)
+        # await self.trigger_send_echo(message_id, self.echo_count[message_id], threshold, payload)
+        await self.trigger_send_echo(message_id, payload)
 
+    # upon event ⟨al,Deliver | p,[ECHO,m]⟩ do
+    #   echos.insert(p)
+    @message_wrapper(EchoMessage)
+    async def on_echo(self, peer: Peer, payload: EchoMessage):
+        
+        self.msg_log.log(LOG_LEVEL.DEBUG, f'Received an ECHO message: {payload.message_id}.')
+        
+        self.echo_count.setdefault(payload.message_id, 0)
+        self.echo_count[payload.message_id] += 1
+
+        # upon event echos.size() ≥ ⌈N+f+1⌉ and not sentReady do
+        threshold = math.ceil((self.f + self.N + 1) / 2)
+        await self.trigger_send_ready(payload.message_id, self.echo_count[payload.message_id], threshold, payload)
+
+    #upon event ⟨al,Deliver | p,[READY,m]⟩ do
+    @message_wrapper(ReadyMessage)
+    async def on_ready(self, peer: Peer, payload: ReadyMessage):
+
+        self.msg_log.log(LOG_LEVEL.DEBUG, f'Received a READY message: {payload.message_id}.')
+
+        self.ready_count.setdefault(payload.message_id, 0)
+        self.ready_count[payload.message_id] += 1
+
+        # upon event readys.size() ≥ f+1 and not sentReady do
+        threshold = self.f + 1
+        await self.trigger_send_ready(payload.message_id, self.ready_count[payload.message_id], threshold, payload)
+
+        # upon event readys.size() ≥ 2f+1 and not delivered do
+        if not self.is_delivered.get(payload.message_id) and \
+            self.ready_count.get(payload.message_id) >= 2 * self.f + 1:
+            self.is_delivered.setdefault(payload.message_id, True)
+
+            self.trigger_delivery(payload)
+
+    """
+    Triggers
+    """
+    ####trigger ⟨al,Send | q,[ECHO,m]⟩ 
+    #upon event ⟨al, Deliver | p, [SEND, m]⟩ and not sentEcho do
+    #   sentEcho = True
+    #   forall q do { trigger ⟨al, Send | q, [ECHO, m]⟩ }
+    async def trigger_send_echo(self, message_id: str, payload: SendMessage):
+
+        sent_echo = self.is_echo_sent.get(message_id, {}).get(self.node_id, False)
+
+        # if not sent_echo and count >= threshold:
+        if not sent_echo:
+            self.is_echo_sent.setdefault(message_id, {})[self.node_id] = True
+            self.msg_log.log(LOG_LEVEL.DEBUG, f'Sent ECHO messages: {message_id}')
+            # Broadcast ECHO message to peers ⟨Dolev,Broadcast|[Echo,m]⟩
+            super().on_broadcast(EchoMessage(payload.message, payload.message_id, self.node_id, []))
+
+    
     # trigger ⟨al,Send | q,[READY,m]⟩
     async def trigger_send_ready(self, message_id: str, count: int, threshold: int, payload: EchoMessage):
         """
@@ -139,59 +171,14 @@ class BrachaRB(BasicDolevRC):
         sent_ready = self.is_ready_sent.get(message_id, {}).get(self.node_id, False)
 
         if not sent_ready and count >= threshold:
-            self.is_ready_sent.setdefault(message_id, {})[self.node_id] = True
-
-            self.msg_log.log(LOG_LEVL.DEBUG, f'Sent READY messages: {message_id}')
-        
-        # Broadcast READY message to peers ⟨Dolev,Broadcast|[ready,m]⟩
-        super().on_broadcast(ReadyMessage(payload.message, payload.message_id, self.node_id, []))
-
-    # upon event ⟨al,Deliver | p,[ECHO,m]⟩ do
-    @message_wrapper(EchoMessage)
-    async def on_echo(self, peer: Peer, payload: EchoMessage):
-        
-        self.msg_log.log(LOG_LEVL.DEBUG, f'Received an ECHO message: {payload.message_id}.')
-        
-        self.echo_count.setdefault(payload.message_id, 0)
-        self.echo_count[payload.message_id] += 1
-
-        # upon event echos.size() ≥ ⌈N+f+1⌉ and not sentReady do
-        threshold = math.ceil((self.f + self.N + 1) / 2)
-        await self.trigger_send_ready(payload.message_id, self.echo_count[payload.message_id], threshold, payload)
-
-            
-    #upon event ⟨al,Deliver | p,[READY,m]⟩ do
-    @message_wrapper(ReadyMessage)
-    async def on_ready(self, peer: Peer, payload: ReadyMessage):
-
-        self.msg_log.log(LOG_LEVL.DEBUG, f'Received a READY message: {payload.message_id}.')
-
-        self.ready_count.setdefault(payload.message_id, 0)
-        self.ready_count[payload.message_id] += 1
-
-        # upon event readys.size() ≥ f+1 and not sentReady do
-        threshold = self.f + 1
-        await self.try_send_ready(payload.message_id, self.ready_count[payload.message_id], threshold, payload)
-
-        # upon event readys.size() ≥ 2f+1 and not delivered do
-        if not self.is_delivered.get(payload.message_id) and \
-            self.ready_count.get(payload.message_id) >= 2 * self.f + 1:
-            self.is_delivered.setdefault(payload.message_id, True)
-
-            self.trigger_delivery(payload)
+            self.is_ready_sent.setdefault(message_id, {})[self.node_id] = True 
+            self.msg_log.log(LOG_LEVEL.DEBUG, f'Sent READY messages: {message_id}')
+            # Broadcast READY message to peers ⟨Dolev,Broadcast|[ready,m]⟩
+            super().on_broadcast(ReadyMessage(payload.message, payload.message_id, self.node_id, []))
 
     # trigger ⟨Bracha,Deliver | s,m⟩
     async def trigger_delivery(self, payload: DolevMessage):
 
-        self.msg_log.log(LOG_LEVL.DEBUG,f'Delivered a message: {payload.message_id}.')
+        self.msg_log.log(LOG_LEVEL.DEBUG,f'Delivered a message: {payload.message_id}.')
 
         await self.msg_log.flush()
-        
-        
-
-        
-
-
-
-        
-        
